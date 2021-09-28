bc037c9439b94b1c709ed1e17c3174605119bf18
Soma Sundaram Sekar <soma.sekar@hellofresh.com>
2021-09-28 19:20:48 +0200

    fix flake8 errors

--- setup.py
+++ setup.py
@@ -3,31 +3,31 @@ import re
 
 from setuptools import find_packages, setup
 
-regexp = re.compile(r'.*__version__ = [\'\"](.*?)[\'\"]', re.S)
+regexp = re.compile(r".*__version__ = [\'\"](.*?)[\'\"]", re.S)
 
-base_package = 'datapipeline_tests'
+base_package = "datapipeline_tests"
 base_path = os.path.dirname(__file__)
 
-init_file = os.path.join(base_path, 'src', 'datapipeline_tests', '__init__.py')
-with open(init_file, 'r', encoding='UTF-8') as f:
+init_file = os.path.join(base_path, "src", "datapipeline_tests", "__init__.py")
+with open(init_file, "r", encoding="UTF-8") as f:
     module_content = f.read()
 
     match = regexp.match(module_content)
     if match:
         version = match.group(1)
     else:
-        raise RuntimeError(
-            f'Cannot find __version__ in {init_file}')
+        raise RuntimeError(f"Cannot find __version__ in {init_file}")
 
-with open('README.rst', 'r', encoding='UTF-8') as f:
+with open("README.rst", "r", encoding="UTF-8") as f:
     readme = f.read()
 
-with open('CHANGELOG.rst', 'r', encoding='UTF-8') as f:
+with open("CHANGELOG.rst", "r", encoding="UTF-8") as f:
     changes = f.read()
 
+
 def parse_requirements(filename):
-    ''' Load requirements from a pip requirements file '''
-    with open(filename, 'r', encoding='UTF-8') as fd:
+    """ Load requirements from a pip requirements file """
+    with open(filename, "r", encoding="UTF-8") as fd:
         lines = []
         for line in fd:
             line.strip()
@@ -35,33 +35,31 @@ def parse_requirements(filename):
                 lines.append(line)
     return lines
 
-requirements = parse_requirements('requirements.txt')
 
+requirements = parse_requirements("requirements.txt")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(
-        name='datapipeline_tests',
-        description='A project to implement data pipeline testing',
-        long_description='\n\n'.join([readme, changes]),
-        license='Not open source',
-        url='https://github.com/somasays/datapipeline_tests',
+        name="datapipeline_tests",
+        description="A project to implement data pipeline testing",
+        long_description="\n\n".join([readme, changes]),
+        license="Not open source",
+        url="https://github.com/somasays/datapipeline_tests",
         version=version,
-        author='Soma Sundaram Sekar',
-        author_email='soma.sekar@hellofresh.com',
-        maintainer='Soma Sundaram Sekar',
-        maintainer_email='soma.sekar@hellofresh.com',
+        author="Soma Sundaram Sekar",
+        author_email="soma.sekar@hellofresh.com",
+        maintainer="Soma Sundaram Sekar",
+        maintainer_email="soma.sekar@hellofresh.com",
         install_requires=requirements,
-        keywords=['datapipeline_tests'],
-        package_dir={'': 'src',
-                     'dags': 'dags'},
-        packages=(
-            find_packages('src') +
-            find_packages(where='./dags')
-        ),
+        keywords=["datapipeline_tests"],
+        package_dir={"": "src", "dags": "dags"},
+        packages=(find_packages("src") + find_packages(where="./dags")),
         zip_safe=False,
-        classifiers=['Development Status :: 3 - Alpha',
-                     'Intended Audience :: Developers',
-                     'Programming Language :: Python :: 3.6',
-                     'Programming Language :: Python :: 3.7',
-                     'Programming Language :: Python :: 3.8']
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+        ],
     )
